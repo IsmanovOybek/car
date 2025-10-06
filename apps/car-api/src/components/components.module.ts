176a890c4e69ff958cdebccaeb288412209e052b
IsmanovOybek <Ismanovoybek75@gmail.com>
2025-10-06 23:18:55 +0900

    fix: modify view module to record visits

--- apps/car-api/src/components/components.module.ts
+++ apps/car-api/src/components/components.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { CarModule } from './property/property.module';
 import { AuthModule } from './auth/auth.module';
+import { ViewModule } from './view/view.module';
 
 @Module({
-  imports: [MemberModule, CarModule, AuthModule]
+  imports: [MemberModule, CarModule, AuthModule, ViewModule]
 })
 export class ComponentsModule {}

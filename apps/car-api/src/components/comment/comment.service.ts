59e7c3189bedbc6854c189ca31ae1b31f1c07788
IsmanovOybek <Ismanovoybek75@gmail.com>
2025-10-19 05:04:33 +0900

    feat: develop getComments graphQL api

--- apps/car-api/src/components/comment/comment.service.ts
+++ apps/car-api/src/components/comment/comment.service.ts
@@ -4,11 +4,13 @@ import { Model, ObjectId } from 'mongoose';
 import { MemberService } from '../member/member.service';
 import { CarService } from '../car/car.service';
 import { BoardArticleService } from '../board-article/board-article.service';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
-import { Message } from '../../libs/enums/common.enum';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
+import { Direction, Message } from '../../libs/enums/common.enum';
 import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
-import { Comment } from '../../libs/dto/comment/comment';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { T } from '../../libs/types/common';
+import { lookupMember } from '../../libs/types/config';
 
 @Injectable()
 export class CommentService {
@@ -76,5 +78,33 @@ export class CommentService {
 		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 
 		return result;
+	}
+
+	public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+		const { commentRefId } = input.search;
+
+		const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+
+		const result: Comments[] = await this.commentModel.aggregate([
+			{ $match: match },
+			{ $sort: sort },
+			{
+				$facet: {
+					list: [
+						{ $skip: (input.page - 1) * input.limit },
+						{ $limit: input.limit },
+						// meLiked (hali qo‘shilmagan)
+						lookupMember,
+						{ $unwind: '$memberData' },
+					],
+					metaCounter: [{ $count: 'total' }],
+				},
+			},
+		]);
+
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result[0];
 	}
 }

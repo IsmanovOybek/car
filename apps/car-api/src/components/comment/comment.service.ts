a18007f15eb3deac59ecbe297e263ab55bb596fb
IsmanovOybek <Ismanovoybek75@gmail.com>
2025-10-19 04:54:26 +0900

    feat: develop updateComment  graphql api

--- apps/car-api/src/components/comment/comment.service.ts
+++ apps/car-api/src/components/comment/comment.service.ts
@@ -6,8 +6,9 @@ import { CarService } from '../car/car.service';
 import { BoardArticleService } from '../board-article/board-article.service';
 import { CommentInput } from '../../libs/dto/comment/comment.input';
 import { Message } from '../../libs/enums/common.enum';
-import { CommentGroup } from '../../libs/enums/comment.enum';
+import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
 import { Comment } from '../../libs/dto/comment/comment';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 
 @Injectable()
 export class CommentService {
@@ -56,6 +57,24 @@ export class CommentService {
 		}
 
 		if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
+		return result;
+	}
+
+	public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
+		const { _id } = input;
+
+		const result = await this.commentModel.findOneAndUpdate(
+			{
+				_id: _id,
+				memberId: memberId,
+				commentStatus: CommentStatus.ACTIVE,
+			},
+			input,
+			{ new: true },
+		);
+
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
 		return result;
 	}
 }

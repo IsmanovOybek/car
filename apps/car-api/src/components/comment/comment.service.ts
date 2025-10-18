b4091b59d4a07669623f776ae6d100f402e86b66
IsmanovOybek <Ismanovoybek75@gmail.com>
2025-10-19 04:51:45 +0900

    feat: develop createComment graphql api

--- apps/car-api/src/components/comment/comment.service.ts
+++ apps/car-api/src/components/comment/comment.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { MemberService } from '../member/member.service';
+import { CarService } from '../car/car.service';
+import { BoardArticleService } from '../board-article/board-article.service';
+import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { Message } from '../../libs/enums/common.enum';
+import { CommentGroup } from '../../libs/enums/comment.enum';
+import { Comment } from '../../libs/dto/comment/comment';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+	constructor(
+		@InjectModel('Comment') private readonly commentModel: Model<Comment>,
+		private memberService: MemberService,
+		private carService: CarService,
+		private boardArticleService: BoardArticleService,
+	) {}
+
+	public async createComment(memberId: ObjectId, input: CommentInput): Promise<Comment> {
+		input.memberId = memberId;
+
+		let result = null;
+		try {
+			result = await this.commentModel.create(input);
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+
+		switch (input.commentGroup) {
+			case CommentGroup.CAR:
+				await this.carService.propertyStatsEditor({
+					_id: input.commentRefId,
+					targetKey: 'carComments',
+					modifier: 1,
+				});
+				break;
+
+			case CommentGroup.ARTICLE:
+				await this.boardArticleService.boardArticleStatsEditor({
+					_id: input.commentRefId,
+					targetKey: 'articleComments',
+					modifier: 1,
+				});
+				break;
+
+			case CommentGroup.MEMBER:
+				await this.memberService.memberStatsEditor({
+					_id: input.commentRefId,
+					targetKey: 'memberComments',
+					modifier: 1,
+				});
+				break;
+		}
+
+		if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
+		return result;
+	}
+}

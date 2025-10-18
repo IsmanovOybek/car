f90a3836b727230760ddc4bada1456a513e30934
IsmanovOybek <Ismanovoybek75@gmail.com>
2025-10-19 01:12:42 +0900

    feat: develop createBoardArticle graphQL api

--- apps/car-api/src/components/board-article/board-article.service.ts
+++ apps/car-api/src/components/board-article/board-article.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { BoardArticle } from '../../libs/dto/board-article/board-article';
+import { MemberService } from '../member/member.service';
+import { ViewService } from '../view/view.service';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class BoardArticleService {
-    constructor(@InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>  ) {}
+    constructor(
+		@InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>,
+		private readonly memberService: MemberService,
+		private readonly viewService: ViewService,
+	) {}
+
+	public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
+		input.memberId = memberId;
+		console.log('Mutation: createBoardArticle');
+
+		try {
+			const result = await this.boardArticleModel.create(input);
+
+			await this.memberService.memberStatsEditor({
+				_id: memberId,
+				targetKey: 'memberArticles',
+				modifier: 1,
+			});
+
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+	}
 }

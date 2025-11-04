634a1aa6d368f1037f33fd3941e556704694c8eb
IsmanovOybek <Ismanovoybek75@gmail.com>
2025-11-04 21:04:09 +0900

    fix: batch module and connect to mongoose

--- apps/car-batch/src/car-batch.module.ts
+++ apps/car-batch/src/car-batch.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CarBatchController } from './car-batch.controller';
 import { CarBatchService } from './car-batch.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [ConfigModule.forRoot(), , DatabaseModule, ScheduleModule.forRoot()],
 	controllers: [CarBatchController],
 	providers: [CarBatchService],
 })

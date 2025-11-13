b8842ee7ecdb50e5bda39aea56f92fdbcdfb949f
IsmanovOybek <Ismanovoybek75@gmail.com>
2025-11-13 15:50:08 +0900

    feat: develop createNotice graphQL api

--- apps/car-api/src/components/notice/notice.resolver.ts
+++ apps/car-api/src/components/notice/notice.resolver.ts
@@ -1,7 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { NoticeService } from './notice.service';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { UseGuards } from '@nestjs/common';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberType } from '../../libs/enums/member.enum';
+import { Notice } from '../../libs/dto/notice/notice';
+import { CreateNoticeInput } from '../../libs/dto/notice/notice.input';
+import { ObjectId } from 'mongoose';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { AuthService } from '../auth/auth.service';
 
 @Resolver()
 export class NoticeResolver {
-    constructor(private readonly noticeService: NoticeService) {}
+	constructor(private readonly noticeService: NoticeService, private readonly authService: AuthService) {}
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Notice)
+	public async createNotice(
+		@AuthMember('_id') memberId: ObjectId,
+		@Args('input') input: CreateNoticeInput,
+	): Promise<Notice> {
+		console.log('Mutation: createNotice');
+		return await this.noticeService.createNotice(memberId, input);
+	}
 }
--- apps/car-api/src/components/notice/notice.service.ts
+++ apps/car-api/src/components/notice/notice.service.ts
@@ -1,10 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { Notice } from '../../libs/dto/notice/notice';
+import { CreateNoticeInput } from '../../libs/dto/notice/notice.input';
+import { NoticeStatus } from '../../libs/enums/notice.enum';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class NoticeService {
-    constructor(
-        @InjectModel('Notice') private noticeModel: Model<void>,
-      ) {}
+	constructor(@InjectModel('Notice') private noticeModel: Model<Notice>) {}
+
+	public async createNotice(memberId: ObjectId, input: CreateNoticeInput): Promise<Notice> {
+		try {
+			const result = await this.noticeModel.create({
+				...input,
+				memberId,
+				noticeStatus: NoticeStatus.ACTIVE,
+			});
+
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+	}
 }

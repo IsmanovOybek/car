0578cd3b4ceae79208003cfc4a8271e00f55c5c9
IsmanovOybek <Ismanovoybek75@gmail.com>
2025-11-13 16:22:11 +0900

    feat: develop getNotices graphQL api

--- apps/car-api/src/components/notice/notice.resolver.ts
+++ apps/car-api/src/components/notice/notice.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { NoticeService } from './notice.service';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { UseGuards } from '@nestjs/common';
@@ -9,10 +9,16 @@ import { CreateNoticeInput } from '../../libs/dto/notice/notice.input';
 import { ObjectId } from 'mongoose';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { AuthService } from '../auth/auth.service';
+import { UpdateNoticeInput } from '../../libs/dto/notice/notice.update';
+import { NoticeCategory } from '../../libs/enums/notice.enum';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
 export class NoticeResolver {
-	constructor(private readonly noticeService: NoticeService, private readonly authService: AuthService) {}
+	constructor(
+		private readonly noticeService: NoticeService,
+		private readonly authService: AuthService,
+	) {}
 
 	@Roles(MemberType.ADMIN)
 	@UseGuards(RolesGuard)
@@ -23,5 +29,22 @@ export class NoticeResolver {
 	): Promise<Notice> {
 		console.log('Mutation: createNotice');
 		return await this.noticeService.createNotice(memberId, input);
+	}
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Notice)
+	public async updateNotice(
+		@Args('input') input: UpdateNoticeInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Notice> {
+		console.log('Mutation: updateNotice');
+		return await this.noticeService.updateNotice(memberId, input);
+	}
+	@UseGuards(WithoutGuard)
+	@Query(() => [Notice])
+	public async getNotices(@Args('input', { type: () => NoticeCategory }) input: NoticeCategory): Promise<Notice[]> {
+		console.log('Query: getNoticeList');
+		return await this.noticeService.getNotices(input);
 	}
 }
--- apps/car-api/src/components/notice/notice.service.ts
+++ apps/car-api/src/components/notice/notice.service.ts
@@ -3,8 +3,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { Notice } from '../../libs/dto/notice/notice';
 import { CreateNoticeInput } from '../../libs/dto/notice/notice.input';
-import { NoticeStatus } from '../../libs/enums/notice.enum';
+import { NoticeCategory, NoticeStatus } from '../../libs/enums/notice.enum';
 import { Message } from '../../libs/enums/common.enum';
+import { UpdateNoticeInput } from '../../libs/dto/notice/notice.update';
 
 @Injectable()
 export class NoticeService {
@@ -23,5 +24,41 @@ export class NoticeService {
 			console.log('Error, Service.model:', err.message);
 			throw new BadRequestException(Message.CREATE_FAILED);
 		}
+	}
+
+	public async updateNotice(memberId: ObjectId, input: UpdateNoticeInput): Promise<Notice> {
+		try {
+			const { noticeId, ...updateData } = input;
+
+			// Hujjatni topish
+			const existNotice = await this.noticeModel.findById(noticeId);
+			if (!existNotice) {
+				throw new BadRequestException('Notice not found');
+			}
+
+			// Yangilanadigan fieldlarni faqat borlarini olish
+			const result = await this.noticeModel.findByIdAndUpdate(
+				noticeId,
+				{
+					...updateData, // title/content/status
+					memberId, // Kim yangilagan
+				},
+				{ new: true },
+			);
+
+			return result;
+		} catch (err) {
+			console.log('Error: updateNotice:', err.message);
+			throw new BadRequestException('Update failed');
+		}
+	}
+
+	 public async getNotices(input: NoticeCategory): Promise<Notice[]> {
+		return await this.noticeModel
+			.find({
+				noticeCategory: input,
+				noticeStatus: NoticeStatus.ACTIVE,
+			})
+			.sort({ createdAt: -1 });
 	}
 }
